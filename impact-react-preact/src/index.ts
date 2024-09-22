c163e642f558c0e39fb4b01da2bc95b32bdf2d63
Christian Alfoni <christianalfoni@gmail.com>
2024-09-22 17:47:28 +0200

    fix preact hmr and bump versions

--- impact-react-preact/src/index.ts
+++ impact-react-preact/src/index.ts
@@ -6,7 +6,7 @@ import { memo } from "react";
 export type { Cleanup } from "@impact-react/store";
 
 export const __observer = (comp: any) => {
-  const component = memo((props) => {
+  const component = memo(function ObserverComponent(props) {
     useSignals();
 
     return comp(props);
